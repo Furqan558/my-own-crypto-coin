db3f5ba1076180dbfc86692a17c9e9a6d377982f
Furqan558 <furqanhameed558@gmail.com>
2023-04-12 10:45:04 +0500

    initialized my coin CryptoDev

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+	"0x9EDdEC08F755e53E718a83fa1Fd13FD0Dc426888";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
